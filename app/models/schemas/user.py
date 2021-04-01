35f5d680b6778898f4b8a7fb490eeae08ecf20c1
samjwillis97 <samjwillis97@gmail.com>
2021-04-01 22:09:20 +1100

    Working on the user models

--- app/models/schemas/user.py
+++ app/models/schemas/user.py
@@ -4,6 +4,7 @@ from pydantic import EmailStr
 
 from base import Base
 from db import DBModelMixin
+from app.core.security import get_password_hash, verify_password
 # from app.core.security
 
 class UserBase(Base):
