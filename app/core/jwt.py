6620bf6bf2bdd221c290b172b26caedf7d4a2947
samjwillis97 <samjwillis97@gmail.com>
2021-04-04 07:48:23 +1000

    changed payload username from sub to user

--- app/core/jwt.py
+++ app/core/jwt.py
@@ -44,7 +44,7 @@ async def get_current_user(
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
+        username: str = payload.get("user")
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
