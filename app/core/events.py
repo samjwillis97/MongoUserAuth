97083b1a0eb7577365ee91e8d99f981f1734b2cd
samjwillis97 <samjwillis97@gmail.com>
2021-04-04 20:53:36 +1000

    fixed annoying loggin

--- app/core/events.py
+++ app/core/events.py
@@ -1,19 +1,13 @@
-import logging
-
 from motor.motor_asyncio import AsyncIOMotorClient
 from .config import MONGODB_URL, MAX_CONNECTIONS_COUNT, MIN_CONNECTIONS_COUNT
 from .mongodb import db
 
 
 async def connect_to_mongo():
-    logging.info("Opening Connection ...")
     db.client = AsyncIOMotorClient(str(MONGODB_URL),
                                    maxPoolSize=MAX_CONNECTIONS_COUNT,
                                    minPoolSize=MIN_CONNECTIONS_COUNT)
-    logging.info("Connetion Opened")
-
+                                   
 
 async def close_mongo_connection():
-    logging.info("Closing Connection ...")
     db.client.close()
-    logging.info("Connection Closed")
